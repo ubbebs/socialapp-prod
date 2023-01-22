51257427ca68deb94dd32e0e9a92001307d27621
ubbebs <strusiu1001@gmail.com>
2023-01-22 01:01:13 +0100

    Link in footer

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -2,9 +2,11 @@ import github from '../../assets/github.png'
 
 export const Footer = () => {
   return (
-    <div className="flex gap-2 items-center justify-center">
-      <p className="text-sm font-semibold">ubbebs</p>
-      <img src={github} alt="github logo" className="h-[25px]" />
-    </div>
+    <a target="_blank" href="https://github.com/ubbebs/" rel="noreferrer">
+      <div className="flex gap-2 items-center justify-center">
+        <p className="text-sm font-semibold">ubbebs</p>
+        <img src={github} alt="github logo" className="h-[25px]" />
+      </div>
+    </a>
   )
 }
